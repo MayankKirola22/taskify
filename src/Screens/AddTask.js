6db4614dd767fc311d6ecd716ba9c5c4fc757aef
Mayank Kirola <69565231+MayankKirola22@users.noreply.github.com>
2024-01-26 23:47:30 +0530

    added self assign restriction

--- src/Screens/AddTask.js
+++ src/Screens/AddTask.js
@@ -7,63 +7,76 @@ import Button from '../Components/Button';
 export function AddTask({user,reload}){
     const [errorMsg, setErrorMsg] = useState("");
     const formatDate= (date) => {
-        let newDate=date.slice(8,10)+'-'+date.slice(5,7)+'-'+date.slice(0,4)
-        return newDate
+                let newDate=date.slice(8,10)+'-'+date.slice(5,7)+'-'+date.slice(0,4)
+                return newDate;
     }
+
     const handleSubmit = (e) => {
         try{
             e.preventDefault();
-            let record=[];
-            if(e.target.AssignMail.value!==""){
-                record={
-                    AssignedBy:user.id,
-                    Comments:[],
-                    Description:e.target.Desc.value,
-                    Status:'Incomplete',
-                    Title:e.target.title.value,
-                    dueDate:formatDate(e.target.dueDate.value)
+            if (new Date(e.target.dueDate.value) >= Date.now()) {
+                let record=[];
+                if(e.target.AssignMail.value!==""){
+                    record={
+                        AssignedBy:user.id,
+                        Comments:[],
+                        Description:e.target.Desc.value,
+                        Status:'Incomplete',
+                        Title:e.target.title.value,
+                        dueDate:formatDate(e.target.dueDate.value)
+                    }
+                }
+                else{
+                    record={
+                        AssignedBy:'',
+                        Comments:[],
+                        Description:e.target.Desc.value,
+                        Status:'Incomplete',
+                        Title:e.target.title.value,
+                        User:user.id,
+                        dueDate:formatDate(e.target.dueDate.value)
                 }
             }
-            else{
-                record={
-                    AssignedBy:'',
-                    Comments:[],
-                    Description:e.target.Desc.value,
-                    Status:'Incomplete',
-                    Title:e.target.title.value,
-                    User:user.id,
-                    dueDate:formatDate(e.target.dueDate.value)
+            if(e.target.AssignMail.value!==''){
+                getByID('Users',localStorage.getItem('isLoggedIn')).then((manager)=>{
+                    let notrec={
+                        Notifications:[...manager.Notifications,`${user.Username} has assigned a task to you.`]
+                    }
+                    getUserIdbyEmail(e.target.AssignMail.value).then(id=>{
+                        if(id!==localStorage.getItem('isLoggedIn')){
+                            updateRecord("Users",id,notrec).then(()=>{
+                                addRecord('Tasks',{...record,User:id}).then(()=>{
+                                    e.target.AssignMail.value='';
+                                    e.target.title.value='';
+                                    e.target.Desc.value='';
+                                    e.target.dueDate.value='';
+                                    setErrorMsg(null);
+                            }
+                            ).catch((err)=>setErrorMsg(err.message))
+                        })
+                    }
+                    else{
+                        setErrorMsg("To self-assign task, Leave \"Assign to\" field empty");
+                    }
+                    }).catch(()=>setErrorMsg("User Not Found. Enter Correct Email ID."))
+                    
+                }).catch((err)=>setErrorMsg(err.message))
+            }else{
+                addRecord('Tasks',record).then(()=>{
+                    e.target.title.value='';
+                    e.target.Desc.value='';
+                    e.target.dueDate.value='';
+                    setErrorMsg(null);
+                }
+                ).catch((err)=>setErrorMsg(err.message))   
             }
         }
-        if(e.target.AssignMail.value!==''){
-            getByID('Users',localStorage.getItem('isLoggedIn')).then((manager)=>{
-                let notrec={
-                    Notifications:[...manager.Notifications,`${user.Username} has assigned a task to you.`]
-                }
-                getUserIdbyEmail(e.target.AssignMail.value).then(id=>{
-                    updateRecord("Users",id,notrec).then(()=>{
-                        addRecord('Tasks',{...record,User:id}).then(()=>{
-                            e.target.AssignMail.value=''
-                            e.target.title.value=''
-                            e.target.Desc.value=''
-                            e.target.dueDate.value=''
-                        }
-                        ).catch((err)=>setErrorMsg(err))
-                    })
-                }).catch(()=>setErrorMsg("User Not Found. Enter Correct Email ID."))
-                
-            }).catch((err)=>setErrorMsg(err))
-        }else{
-            addRecord('Tasks',record).then(()=>{
-                e.target.title.value=''
-                e.target.Desc.value=''
-                e.target.dueDate.value=''
-            }
-            ).catch((err)=>setErrorMsg(err))   
+        else{
+            setErrorMsg("Deadline can not be less than current date.")
         }
     }
     catch{
-        setErrorMsg('An Error occured.')
+        setErrorMsg("An Error Occured.")
     }
     reload();
     }
